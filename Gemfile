e01f5ae4aedcc1e702334ded744f2af90f66000a
Kevin Lavier <kevinlavier@orange.fr>
2025-03-21 13:54:37 +0100

    Initialize project

--- Gemfile
+++ Gemfile
@@ -1,5 +1,13 @@
 source "https://rubygems.org"
 
 gem 'sinatra', '4.0.0'
+gem "sinatra-activerecord"
 gem "rackup"
 gem "puma"
+gem 'sqlite3'
+gem 'rake'
+gem 'bcrypt'
+gem 'json'
+gem 'sinatra-contrib'
+gem 'rack-livereload'
+gem 'shotgun'
--- app.rb
+++ app.rb
@@ -1,10 +1,19 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'rack-livereload' if development?
+require 'sinatra/activerecord'
+require 'bcrypt'
+
 
 # Utiliser le port défini par l'environnement ou 8080 par défaut
 set :port, ENV.fetch("PORT", 8080)
 # Faire écouter l'application sur toutes les interfaces
 set :bind, '0.0.0.0'
 
+# Configure SQLite3 pour l'environnement de développement
+set :database, { adapter: "sqlite3", database: "db/development.sqlite3" }
+
+# Route pour la page d'accueil
 get '/' do
-  "Hello, world!"
+  erb :index
 end
